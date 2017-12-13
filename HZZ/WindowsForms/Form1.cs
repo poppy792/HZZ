a7763f6b49cc681b68c11f213127eb6980cc4ab7
Domagoj Peršić <domagoj.persic@vsmti.hr>
2017-12-13 17:48:42 +0100

    Task 1. i 2.

--- HZZ/WindowsForms/Form1.cs
+++ HZZ/WindowsForms/Form1.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using REST_api;
 
 namespace WindowsForms
 {
     public partial class Form1 : Form
     {
+        public List<Button> button = new List<Button>();
+        public List<Category> lCategories = new List<Category>();
         public Form1()
         {
             InitializeComponent();
+
+
+            List<Button> button = new List<Button>();
+            for (int i = 0; i<10; i++)
+            {
+                Button newButton = new Button();
+                button.Add(newButton);
+                this.Controls.Add(newButton);
+            }
+
         }
     }
 }
